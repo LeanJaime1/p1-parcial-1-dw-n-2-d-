616e1f3b13d5e65117178468cee9f4c2c5403d38
LeanJaime1 <102166092+LeanJaime1@users.noreply.github.com>
2022-11-06 11:04:57 -0300

    Creo las clases Disco y Pista

--- index.js
+++ index.js
@@ -2,3 +2,21 @@
  * APELLIDO, NOMBRE | APELLIDO, NOMBRE
  */
 
+//creamos las clases
+
+class Disco {
+    constructor(nombreDisco, autorDisco, codigoDisco, ){
+        this.nombreDisco = nombreDisco;
+        this.autorDisco = autorDisco;
+        this.codigoDisco = codigoDisco;
+
+    }
+}
+
+class Pista{
+    constructor(nombrePista, duracionPista){
+        this.nombrePista = nombrePista;
+        this.duracionPista = duracionPista
+    }
+}
+
